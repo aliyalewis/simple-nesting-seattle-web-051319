f4e685e0c2928afe05fddb851df8e12deae1d608
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-07 17:02:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,6 +121,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennise][:languages] << "Assembly"
-programmer_hash
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
